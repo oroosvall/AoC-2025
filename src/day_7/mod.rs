39196c1dee96171a017e414e5db0d1dae65c4a33
Oscar Roosvall <10653734+oroosvall@users.noreply.github.com>
2025-12-07 18:38:00 +0100

    Optimize day 7

--- src/day_7/mod.rs
+++ src/day_7/mod.rs
@@ -1,7 +1,13 @@
 use std::collections::{HashMap, HashSet, VecDeque};
 
 pub fn part_1(vec: &Vec<String>) -> u64 {
-    let mut deq: VecDeque<&String> = VecDeque::from_iter(vec);
+    let v: Vec<_> = vec
+        .iter()
+        .enumerate()
+        .filter(|&(i, _)| i % 2 == 0)
+        .map(|(_, v)| v)
+        .collect();
+    let mut deq = VecDeque::from_iter(v);
 
     let mut lines = HashSet::new();
     lines.insert(deq.pop_front().unwrap().find('S').unwrap());
@@ -26,7 +32,13 @@ pub fn part_1(vec: &Vec<String>) -> u64 {
 }
 
 pub fn part_2(vec: &Vec<String>) -> usize {
-    let mut deq: VecDeque<&String> = VecDeque::from_iter(vec);
+    let v: Vec<_> = vec
+        .iter()
+        .enumerate()
+        .filter(|&(i, _)| i % 2 == 0)
+        .map(|(_, v)| v)
+        .collect();
+    let mut deq = VecDeque::from_iter(v);
 
     let mut lines = HashMap::new();
     lines.insert(deq.pop_front().unwrap().find('S').unwrap(), 1);
@@ -35,8 +47,14 @@ pub fn part_2(vec: &Vec<String>) -> usize {
         let mut new_lines = HashMap::new();
         for (l, tl) in lines {
             if row.chars().nth(l).unwrap() == '^' {
-                new_lines.entry(l - 1).and_modify(|e| *e += tl).or_insert(tl);
-                new_lines.entry(l + 1).and_modify(|e| *e += tl).or_insert(tl);
+                new_lines
+                    .entry(l - 1)
+                    .and_modify(|e| *e += tl)
+                    .or_insert(tl);
+                new_lines
+                    .entry(l + 1)
+                    .and_modify(|e| *e += tl)
+                    .or_insert(tl);
                 // splits += 1;
             } else {
                 new_lines.entry(l).and_modify(|e| *e += tl).or_insert(tl);
